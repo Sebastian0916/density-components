6a1f874bb1f97f7b9466a16220ac78a06a8706b2
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-02-07 15:17:25 -0500

    Cambios tamaños y directiva

--- densityComponents/src/app/app.module.ts
+++ densityComponents/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent            } from './app.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule          } from './material.module';
+import { MaterialModule } from './material.module';
+import { SizeDirective } from 'src/theme/size.directive';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    MaterialModule,   
-    BrowserModule,
-    BrowserAnimationsModule,
-  ],
+  declarations: [AppComponent,SizeDirective],
+  imports: [MaterialModule, BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- densityComponents/src/app/material.module.ts
+++ densityComponents/src/app/material.module.ts
@@ -1,26 +1,31 @@
-import { NgModule                 } from '@angular/core';
-import { CommonModule             } from '@angular/common';
-import { MatButtonModule          } from '@angular/material/button';
-import { MatProgressBarModule     } from '@angular/material/progress-bar';
-import { MatChipsModule           } from '@angular/material/chips';
-import { MatToolbarModule         } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSliderModule          } from '@angular/material/slider';
-import { MatBadgeModule           } from '@angular/material/badge';
-import { MatSlideToggleModule     } from '@angular/material/slide-toggle';
-import { MatStepperModule         } from '@angular/material/stepper';
-import { MatInputModule           } from '@angular/material/input';
-import { MatSnackBarModule        } from '@angular/material/snack-bar';
-import { MatIconModule            } from '@angular/material/icon';
-import { MatTabsModule            } from '@angular/material/tabs';
-import { MatCheckboxModule        } from '@angular/material/checkbox';
-import { MatRadioModule           } from '@angular/material/radio';
-import { MatDividerModule         } from '@angular/material/divider';
-import { MatFormFieldModule       } from '@angular/material/form-field';
-import { MatSelectModule          } from '@angular/material/select';
-import { MatDatepickerModule      } from '@angular/material/datepicker';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatMenuModule } from '@angular/material/menu';
+
 const components = [
+  MatMenuModule,
+  MatAutocompleteModule,
   CommonModule,
   MatButtonModule,
   MatProgressBarModule,
@@ -42,13 +47,12 @@ const components = [
   MatSelectModule,
   MatDatepickerModule,
   MatRippleModule,
-  MatNativeDateModule
-  
+  MatNativeDateModule,
 ];
 
 @NgModule({
   declarations: [],
   imports: [components],
-  exports: [components]
+  exports: [components],
 })
 export class MaterialModule {}
